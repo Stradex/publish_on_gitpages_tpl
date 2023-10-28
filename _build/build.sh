0399d9d7de21d8d8f72dc33f7a891adf023dc855
devel@mauriciocap.com <devel@mauriciocap.com>
2023-10-28 18:44:44 -0400

    TRY: plantuml

--- _build/build.sh
+++ _build/build.sh
@@ -1,4 +1,18 @@
-#!/usr/bin/bash
+!/usr/bin/bash
 
-mkdir -p out
+which make
+
+OUT_DIR="`pwd`/out"
+mkdir -p $OUT_DIR
 (echo "<pre>" ; find ; echo "</pre>") > out/index.html
+
+echo "BUILD Colab/Jupyter"
+pip install jupyter nbconvert
+jupyter nbconvert -y --output-dir=./_build/html --to html --template=tpl --theme=dark *.ipynb
+
+echo "BUILD plantuml"
+curl -Lo plantuml.jar 'https://github.com/plantuml/plantuml/releases/download/v1.2023.12/plantuml-1.2023.12.jar'
+java -jar plantuml.jar -o "$OUT_DIR" -tsvg -darkmode _build/test*.uml
+
+echo "BUILD Next"
+
