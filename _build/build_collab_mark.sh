03385c06a209cd6c741d930f0217c1ff1dd47e54
Stradex <stradexengine@gmail.com>
2023-10-31 10:44:39 -0300

    FIX: Copy content from articulos folder into NextJS articulos folder correctly

--- _build/build_collab_mark.sh
+++ _build/build_collab_mark.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+# Installing jupyter nbconvert so we can convert Google Collab files into html
 pip install jupyter nbconvert
 
-LOCAL_POSTS="`pwd`/articulos"
+LOCAL_POSTS="`pwd`/articulos/*"
 NEXTJS_POSTS="`pwd`/site/src/app/articulos"
 PAGEJS_TEMPLATE="`pwd`/site/src/app/_blog_template.js"
 
-# Copying files from local posts folder into NextJS project posts folder
+# Copying files from local articulos folder into NextJS project articulos folder
 cp -r "$LOCAL_POSTS"* $NEXTJS_POSTS
-
-
 
 # Check if the target is not a directory
 if [ ! -d "$NEXTJS_POSTS" ]; then
   exit 1
 fi
 
+# Iterate through NextJS articulos folder so we can convert the necessary Google Collab files
+# And also rename the necessary page.mdx files
 function iterate() {
   local dir="$1"
 
@@ -38,29 +39,5 @@ function iterate() {
 }
 
 iterate $NEXTJS_POSTS
-
-
-# Loop through files in the target directory
-# for file in "$NEXTJS_POSTS"/*; do
-#   if [ -f "$file" ]; then
-#     filename=$(basename "$file")
-#     filename_no_ext="${filename%.*}"
-#     extension="${filename##*.}"
-#     dest_folder=$OUT_DIR/"$filename_no_ext"
-#     if [ "$extension" == "mdx" ]; then
-#         mkdir -p "$dest_folder"
-#         cp "$file" "$dest_folder/page.mdx"
-#     elif [ "$extension" == "md" ]; then
-#         mkdir -p "$dest_folder"
-#         cp "$file" "$dest_folder/page.mdx"
-#     elif [ "$extension" == "ipynb" ]; then
-#         mkdir -p "$dest_folder"
-#         cp $PAGEJS_TEMPLATE "$dest_folder/page.js"
-#         jupyter nbconvert -y --output-dir=$dest_folder --to html --template=../colab_web_tpl/tpl --theme=dark --output jupyter.html $file
-#     fi
-#   fi
-# done
-
-# cd ..
 
 echo "BUILD COLLAB AND MARKDOWN OK"
